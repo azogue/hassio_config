5da78e1448f5ccb499f89ac23ca394db12b5b0a2
azogue <eugenio.panadero@gmail.com>
2019-03-05 20:21:07 +0100

    New script for quick heat AC session

--- config/python_scripts/set_cool_ac_session.py
+++ config/python_scripts/set_cool_ac_session.py
@@ -1,4 +1,8 @@
-CLIMATE_ENTITY = 'climate.termostato_ac'
+operation_mode = data.get("mode")
+cool_mode = operation_mode == 'cool'
+
+entity_name = 'termostato_ac' if cool_mode else 'calefaccion'
+CLIMATE_ENTITY = f'climate.{entity_name}'
 AUTO_OFF_ENTITY = 'automation.apagado_de_ac'
 TELEGRAM_TARGET = 'sensor.telegram_default_chatid'
 
@@ -17,29 +21,37 @@ if target_temp > current_temp - .5:
     hass.services.call(
         'climate', 'set_temperature',
         {"entity_id": CLIMATE_ENTITY,
-         "operation_mode": "cool",
+         "operation_mode": operation_mode,
          "temperature": new_temp})
 
 if state_climate.state == 'off':
     # Set operation mode to cool if necessary
     hass.services.call('climate', 'set_operation_mode',
-                       {"entity_id": CLIMATE_ENTITY, "operation_mode": "cool"})
+                       {"entity_id": CLIMATE_ENTITY,
+                        "operation_mode": operation_mode})
 
 # Turn on automation to turn off climate when finished
 hass.services.call('automation', 'turn_on', {"entity_id": AUTO_OFF_ENTITY})
+
+message = ("Encendido de AC por ciclo único de calentamiento desde "
+           "{{ states.climate.calefaccion."
+           "attributes.current_temperature }} °C hasta que llegue a "
+           "{{ states.climate.calefaccion.attributes.temperature + 1}}"
+           " °C (con encendido de AUTO-OFF)")
+if cool_mode:
+    message = (message
+               .replace(' + 1', ' - 1')
+               .replace('calefaccion', 'termostato_ac')
+               .replace('calentamiento', 'enfriamiento'))
 
 # Telegram bot notify with undo command
 target = int(hass.states.get(TELEGRAM_TARGET).state)
 hass.services.call(
     'telegram_bot', 'send_message',
     {"title": "*Comienzo de ciclo de AC*",
-     "message": "Encendido de AC por ciclo único de enfriamiento desde "
-                "{{ states.climate.termostato_ac."
-                "attributes.current_temperature }} °C hasta que llegue a "
-                "{{ states.climate.termostato_ac.attributes.temperature - 1}}"
-                " °C (con encendido de AUTO-OFF)",
+     "message": message,
      # "target": "{{ states.sensor.telegram_default_chatid.state | int }}",
      "target": target,
      "disable_notification": True,
-     "inline_keyboard": ["Deshacer:/service_call climate.set_operation_mode "
-                         "termostato_ac off"]})
+     "inline_keyboard": [f"Deshacer:/service_call climate.set_operation_mode "
+                         f"{entity_name} off"]})
