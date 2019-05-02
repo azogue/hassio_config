4dfea9b007491c7c410e19bbac864939339d2071
azogue <eugenio.panadero@gmail.com>
2019-05-02 10:37:43 +0200

    update HA init script

--- config/python_scripts/start_states.py
+++ config/python_scripts/start_states.py
@@ -6,19 +6,11 @@ This unifies various automations and HA scripts in a simpler one.
 
 """
 
-# Anyone at home?
-family_home = hass.states.get('group.family').state == 'home'
-
-# Turn on default outlets
-# if family_home:
-#     hass.services.call(
-#         'switch', 'turn_on',
-#         {"entity_id": "switch.tv_power,switch.camara,switch.vinoteca"})
-        # {"entity_id": "switch.calentador,switch.bomba_circ_acs"})
-
 # Create motioneye binary_sensors
 cameras = {
-    'binary_sensor.motioncam_salon': "Vídeo-Mov. en Salón",
+    'binary_sensor.motioncam_pizero': "Vídeo-Mov. en PIzero",
+    'binary_sensor.motioncam_pizero2': "Vídeo-Mov. en PIW2",
+    # 'binary_sensor.motioncam_salon': "Vídeo-Mov. en Salón",
     'binary_sensor.motioncam_terraza': "Vídeo-Mov. en Terraza",
     'binary_sensor.motioncam_office': "Vídeo-Mov. en Estudio",
 }
@@ -26,28 +18,13 @@ for bs, fn in cameras.items():
     hass.states.set(bs, 'off',
                     attributes={
                         "friendly_name": fn,
-                        "homebridge_hidden": "true",
                         "device_class": "motion"})
-
-# Sync HA dev trackers with manual HomeKit input_booleans
-dev_tracking = {'group.eugenio': 'input_boolean.eu_presence',
-                'group.mary': 'input_boolean.carmen_presence'}
-for group in dev_tracking:
-    input_b = dev_tracking.get(group)
-    b_in_home = hass.states.get(group).state == 'home'
-    input_b_st = hass.states.get(input_b)
-    input_b_in_home = input_b_st.state == 'on'
-    if input_b_in_home != b_in_home:
-        logger.warning('SYNC error %s: dev_tracker=%s, HomeKit=%s',
-                       group.lstrip('group.'), b_in_home, input_b_in_home)
-        hass.states.set(input_b, "on" if b_in_home else "off",
-                        attributes=input_b_st.attributes)
 
 # Notify HA init with iOS
 hass.services.call(
-    'notify', 'ios_iphone',
+    'notify', 'ios_iphone_beta',
     {"title": "Home-assistant started",
-     "message": "Hass is now ready for you",
+     "message": "Hassio is now ready for you",
      "data": {"push": {"badge": 5,
                        "sound": "US-EN-Morgan-Freeman-Welcome-Home.wav",
                        "category": "CONFIRM"}}})
