ca22b6b7272b4ab3b40c756beb7567d97bac1575
azogue <eugenio.panadero@gmail.com>
2020-02-10 09:39:56 +0100

    Use custom event to detect hue switch clicks
    even when state is not changed (button repetition)

--- config/appdaemon/apps/hue_tap_controller.py
+++ config/appdaemon/apps/hue_tap_controller.py
@@ -26,22 +26,30 @@ class HueTapControl(hass.Hass):
 
     def initialize(self):
         """Set up appdaemon app."""
-        self.listen_state(self._tap_used, TAP_SENSOR)
+        self.listen_event(self._tap_event_triggered, "hue_tap_triggered")
 
-    def _tap_used(self, entity, attribute, old, new, kwargs):
-        if new == "1_click":  # toggle terraza
+    def _tap_event_triggered(self, event_name, _event_data, *_args, **_kwargs):
+        tap_code = self.get_state(TAP_SENSOR)
+        if tap_code == "1_click":  # toggle terraza
             self.call_service("light/toggle", entity_id=LIGHT_TOGGLE)
             self.log(
-                f"Tap was used: {new} (before it was {old}) "
+                f"Tap was used: {tap_code}"
                 f"--> terraza is {self.get_state(LIGHT_TOGGLE)}",
                 log=LOGGER,
             )
-        elif new == "2_click":  # adjust cover ventanal
-            st_cover_pos = int(
-                self.get_state(COVER_WINDOW, attribute="attributes")[
-                    "current_position"
-                ]
-            )
+        elif tap_code == "3_click":  # adjust ambilight
+            st_amb = self.get_state(SWITCH_AMBI)
+            if st_amb == "off":
+                self.call_service("switch/turn_on", entity_id=SWITCH_AMBI)
+                self.log(f"Adjust Hue+ambilight to ON", log=LOGGER)
+            else:
+                self.call_service("switch/turn_off", entity_id=SWITCH_AMBI)
+                self.call_service("light/turn_on", **LIGHT_PARAMS_AFTER_AMBI)
+                self.log(f"Adjust Hue+ambilight to OFF", log=LOGGER)
+
+        elif tap_code == "2_click":  # adjust cover ventanal
+            cover_attrs = self.get_state(COVER_WINDOW, attribute="attributes")
+            st_cover_pos = int(cover_attrs["current_position"])
             if st_cover_pos > 90:
                 # set position 30
                 self.call_service(
@@ -59,21 +67,9 @@ class HueTapControl(hass.Hass):
                 self.log(
                     f"OPEN cover ventanal (was {st_cover_pos})", log=LOGGER,
                 )
-        elif new == "3_click":  # adjust ambilight
-            st_amb = self.get_state(SWITCH_AMBI)
-            if st_amb == "off":
-                self.call_service("switch/turn_on", entity_id=SWITCH_AMBI)
-                self.log(f"Adjust Hue+ambilight to ON", log=LOGGER)
-            else:
-                self.call_service("switch/turn_off", entity_id=SWITCH_AMBI)
-                self.call_service("light/turn_on", **LIGHT_PARAMS_AFTER_AMBI)
-                self.log(f"Adjust Hue+ambilight to OFF", log=LOGGER)
-        elif new == "4_click":  # adjust cover puerta
-            st_cover_pos = int(
-                self.get_state(COVER_DOOR, attribute="attributes")[
-                    "current_position"
-                ]
-            )
+        elif tap_code == "4_click":  # adjust cover puerta
+            cover_attrs = self.get_state(COVER_DOOR, attribute="attributes")
+            st_cover_pos = int(cover_attrs["current_position"])
             if st_cover_pos > 90:
                 # set position 60
                 self.call_service(
