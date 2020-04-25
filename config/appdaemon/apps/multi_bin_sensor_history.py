fe38f22232d8e3878a102dc1aab5c34121b12312
azogue <eugenio.panadero@gmail.com>
2020-04-25 14:44:08 +0200

    Enhance AppDaemon for Binary sensor history
    - State with just location, not time
    - State restoration on init
    - Reverse history (0 + recent)

--- config/appdaemon/apps/multi_bin_sensor_history.py
+++ config/appdaemon/apps/multi_bin_sensor_history.py
@@ -5,7 +5,7 @@ Binary sensor custom history as AppDaemon App for Home Assistant.
 Listen to multiple binary sensors to reflect the last activation,
 maintaining a history of the lasts activations.
 
-* Sensor State: `Friendly name: activation_timestamp`
+* Sensor State: `Friendly name`
 * Sensor attributes:
     `history_1: Friendly name: activation_timestamp`,
     `history_2: Friendly name: activation_timestamp`,
@@ -33,6 +33,8 @@ from typing import Deque, Dict
 
 import appdaemon.plugins.hass.hassapi as hass
 
+_ATTR_NAME = "history_"
+
 
 class MultiBinSensor(hass.Hass):
 
@@ -51,8 +53,7 @@ class MultiBinSensor(hass.Hass):
         self._date_format = self.args.get("format_last_changed")
 
         # Set up state history for attributes
-        max_history = int(self.args.get("max_history"))
-        self._history = deque([], maxlen=max_history)
+        self._history = deque([], maxlen=int(self.args.get("max_history")))
 
         # Listen for binary sensor activations and store friendly names for them
         bin_sensors: Dict[str, str] = self.args.get("binary_sensors")
@@ -61,24 +62,33 @@ class MultiBinSensor(hass.Hass):
             self._friendly_names[sensor] = pretty_name
             self.listen_state(self._bin_sensor_activation, sensor, new="on")
 
-    def _get_attributes(self) -> dict:
-        attributes = {**self._entity_attributes}
-        attributes.update(
-            {f"history_{i + 1}": state for i, state in enumerate(self._history)}
-        )
-        return attributes
+        # recover old values, if any
+        old_attrs = self.get_state(self._entity, attribute="all")
+        if old_attrs:
+            state = old_attrs.get("state", "unknown")
+            for k, old_value in reversed(old_attrs.get("attributes", {}).items()):
+                if k.startswith(_ATTR_NAME) and ": " in old_value:
+                    self._history.append(old_value)
+            # Re-Publish old state
+            self._set_new_sensor_state(state)
 
-    def _set_new_history_state(self, entity):
-        """Generate a new state, update history and publish it."""
-        pretty_date_now = self.datetime().strftime(self._date_format)
-        state = f"{self._friendly_names[entity]}: {pretty_date_now}"
-
-        # Add to history
-        self._history.append(state)
-
-        # Publish new state
-        self.set_state(self._entity, state=state, attributes=self._get_attributes())
+    def _set_new_sensor_state(self, state):
+        """Publish a new state for the sensor."""
+        history_attrs = {
+            f"{_ATTR_NAME}{i}": old_state
+            for i, old_state in enumerate(reversed(self._history))
+            if i > 0
+        }
+        attributes = {**self._entity_attributes, **history_attrs}
+        self.set_state(self._entity, state=state, attributes=attributes)
 
     def _bin_sensor_activation(self, entity, attribute, old, new, kwargs):
-        """Listen to binary sensors turning on."""
-        self._set_new_history_state(entity)
+        """Listen to bin sensors turning on, update history and publish a new state."""
+        location = self._friendly_names[entity]
+        pretty_date_now = self.datetime().strftime(self._date_format)
+
+        # Add to history
+        self._history.append(f"{location}: {pretty_date_now}")
+
+        # Publish new state
+        self._set_new_sensor_state(location)
